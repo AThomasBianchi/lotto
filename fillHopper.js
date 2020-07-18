8193c4980e615dc880d05a75779023949e494cba
A Thomas Bianchi <atbianchi@gmail.com>
2020-07-18 12:10:55 -0400

    add testing, update node

--- fillHopper.js
+++ fillHopper.js
@@ -1,16 +1,8 @@
-let lottoBalls = {
-  tom: 5,
-  weist: 4,
-  guenther: 1
-}
-
-const getBallz = (league) => {
+export const getBallz = (league) => {
   const ballzArray = [];
   for (const [key, value] of Object.entries(league)) {
     let teamArray = new Array(value).fill(key)
     ballzArray.push(...teamArray);
   }
   return ballzArray;
-}
-
-console.log(getBallz(lottoBalls));
+};
--- test/test.fillHopper.js
+++ test/test.fillHopper.js
@@ -1,8 +1,12 @@
-var assert = require('assert');
+import { equal } from 'assert';
+import { getBallz } from '../fillHopper.js';
+
+console.log(getBallz);
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+      equal([1, 2, 3].indexOf(4), -1);
     });
   });
 });
